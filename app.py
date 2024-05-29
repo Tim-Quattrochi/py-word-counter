a01d06bdaf3b132c03a5adccc0685fb08bef4998
Tim Quattrochi <timq82@gmail.com>
2024-05-28 21:03:24 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# 1. Implement a hash function.
+
+# 2. Implement a hash table with chaining
+# - insert(key, value)
+# - lookup(key) -> value
+# - delete(key)
+# __len__ () => int
+# - resize(int)
+# - items() -> list or generator of [key, value] pairs
+
+# 3. Use hash table to count word frequencies in a book.
