36ad287be6324c78d616f18a7c5ee05aeb891e8d
Tim Quattrochi <timq82@gmail.com>
2024-06-07 19:23:46 -0400

    chore: separate hash class and hash fn, update gitignore

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from hash_class import MyHashTable
 # Reference slides: https://docs.google.com/presentation/d/14N_E4uZL6XqT4bejTkRYDq7sps-hBxN8VQTGXJCpGxg/edit#slide=id.p
 
 # 1. Implement a hash function.
@@ -14,66 +15,15 @@
 # 3. Use hash table to count word frequencies in a book.
 
 # djb2 hash function implementation.
-def djb2(key):
-    hash_value = 5381
-    for char in key:
-        # "<<" bitwise left shift operator. It shifts the bits of its left-hand operand to the left by the number of positions specified by its right-hand operand.
-        # "ord()" returns the Unicode code point for a given character.
-        hash_value = ((hash_value << 5) + hash_value) + ord(char)
-    # '&' is used for bitwise operations, 'and' is for logical.
-    return hash_value & 0xFFFFFFFF
 
 
-class MyHashTable:
-    def __init__(self, size=10):
-        self.size = size
-        self.table = [[] for _ in range(size)]
-
-    def insert(self, key, value):
-        hash_value = self._hash(key)
-        idx = hash_value % len(self.table)
-
-        for pair in self.table[idx]:
-            if pair[0] == key:
-                pair[1] = value
-                return
-            # not found, add it.
-        self.table[idx].append([key, value])
-
-    def get_value(self, key):
-        hash_value = self._hash(key)
-        idx = hash_value % len(self.table)
-
-        for pair in self.table[idx]:
-            if pair[0] == key:
-                return pair[1]
-
-        raise KeyError(key)
-
-    def delete(self, key):
-        hash_value = self._hash(key)
-        idx = hash_value % len(self.table)
-
-        for i, pair in enumerate(self.table[idx]):
-            if pair[0] == key:
-                del self.table[idx][i]
-                return
-
-        raise KeyError(key)
-
-    def _hash(self, key):
-        return djb2(key)
-
-    # method for inspecting table.
-    def __str__(self):
-        return str(self.table)
+def main():
+    hash_table = MyHashTable(20)
+    hash_table.insert("hello", "there")
+    print(hash_table.get_value("hello"))
+    # hash_table.delete("hello")
+    print(hash_table)
 
 
-hash_table = MyHashTable(20)
-
-insert = hash_table.insert("hello", "there")
-get_val_frm_table = hash_table.get_value("hello")
-# delete = hash_table.delete("hello")
-
-
-print(hash_table)
+if __name__ == "__main__":
+    main()
