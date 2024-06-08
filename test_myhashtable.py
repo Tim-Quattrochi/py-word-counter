4cb762efbb0c326c785bf519096ae385465b8f2f
Tim Quattrochi <timq82@gmail.com>
2024-06-08 17:57:38 -0400

    test: add test for collision and size

--- test_myhashtable.py
+++ test_myhashtable.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 from hash_class import MyHashTable
 
 
@@ -16,6 +17,26 @@ class TestMyHashTable(unittest.TestCase):
         self.assertEqual(hash_table.get_value("hello world"), 5)
         self.assertEqual(hash_table.get_value("python"), 10)
         self.assertEqual(hash_table.get_value("tensorflow"), 15)
+
+    def test_size(self):
+        # test default size
+        self.assertEqual(MyHashTable().size, 10)
+
+        hash_table = MyHashTable(30000)
+        self.assertEqual(hash_table.size, 30000)
+
+    def test_collision(self):
+        with patch.object(MyHashTable, '_hash', return_value=0):
+
+            hash_table = MyHashTable(10)
+
+        # insert
+            hash_table.insert("hello world", 5)
+            hash_table.insert("python", 10)
+
+        # assertions
+            self.assertEqual(hash_table.get_value("hello world"), 5)
+            self.assertEqual(hash_table.get_value("python"), 10)
 
 
 if __name__ == '__main__':
