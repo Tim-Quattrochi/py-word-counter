1bd60df284ed0363cd2441d28f37dc9ea2f517f3
Tim Quattrochi <timq82@gmail.com>
2024-06-07 21:47:43 -0400

    feat: Add frequency counter function for top 10 words

--- app.py
+++ app.py
@@ -36,6 +36,16 @@ def process_data(file):
         return data
 
 
+def frequency_counter(table_items_fn):
+    items = list(table_items_fn())
+    items.sort(key=lambda pair: pair[1], reverse=True)
+    print("Top 10 words: \t\n")
+    print("{:<15} {:<10}".format('Word', 'Frequency'))
+    print('-' * 25)
+    for word, frequency in items[:10]:
+        print("{:<15} {:<10}".format(word, frequency))
+
+
 def main():
     hash_table = MyHashTable(30000)
     words = process_data(book_path).split()
@@ -44,7 +54,8 @@ def main():
             hash_table.insert(word, hash_table.get_value(word) + 1)
         except KeyError:  # if the word is not found, add it.
             hash_table.insert(word, 1)
-    print(hash_table)
+
+    frequency_counter(hash_table.items)
 
 
 if __name__ == "__main__":
